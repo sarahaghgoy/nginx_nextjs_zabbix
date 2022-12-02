a5a45fba92dcd07a2d72a696537020ec174f8ca1
root <root@localhost.localdomain>
2022-12-02 11:55:24 +0000

    Docker containers,Docker compose,And Zabbix server webfrontend Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Script for debian
+
+#Insatall docker and docker compose
+sudo apt-get update -y
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y 
+
+#Run Docker compose 
+cd Docker
+docker compose up -d
+
